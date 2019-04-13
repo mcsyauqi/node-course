b41f6f610e275beab010012ef95b265d846fcc2b
Unknown <ahmadthariqsyauqi@gmail.com>
2019-04-13 12:18:47 +0700

    ~22. Refactoring

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,16 +1,19 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function () {
+const getNotes = () => {
     return 'Your notes...'
 }
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
 
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+    // const duplicateNotes = notes.filter(function (note) {
+    //     return note.title === title
+    // })
+
+    const duplicateNotes = notes.filter((note) => notes.title)
+
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -24,12 +27,11 @@ const addNote = function (title, body) {
     }
 }
 
-const removeNote = function (title) {
+const removeNote = (title) => {
     const notes = loadNotes()
 
-    const notesToKeep = notes.filter(function (note) {
-        return note.title !== title
-    })
+    const notesToKeep = notes.filter((note) =>
+        note.title !== title)
 
     if (notesToKeep.length < notes.length) {
         saveNotes(notesToKeep)
@@ -39,12 +41,12 @@ const removeNote = function (title) {
     }
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -6,6 +6,21 @@
 //     return x * x
 // }
 
-const square = (x) => x * x
+// const square = (x) => x * x
 
-console.log(square(5))
+// console.log(square(5))
+
+const event = {
+    name: 'Birthday Party',
+    guestList: ['Andrew', 'Jen', 'Mike'],
+    printGuestList() {
+        console.log('Guest list for ' + this.name)
+
+        this.guestList.forEach((guest) => {
+            console.log(guest + ' is attending ' + this.name)
+        })
+    }
+
+}
+
+event.printGuestList()
