e4e4993511b99201b08491e02419a7070753dc25
Unknown <ahmadthariqsyauqi@gmail.com>
2019-04-15 14:19:33 +0700

    32. Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,10 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/17ae17408409e68c79f65c4360ca5d53/37.8267,-122.4233?units=si&lang=id'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0);
-console.log('Stopping')
+request({
+    url: url,
+    json: true
+}, (error, response) => {
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+})
