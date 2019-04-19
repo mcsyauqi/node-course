00862b24cc273acc078e312bf3fab6280301abb5
Unknown <ahmadthariqsyauqi@gmail.com>
2019-04-19 15:47:41 +0700

    34. Handling Errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,34 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/17ae17408409e68c79f65c4360ca5d53/37.8267,-122.4233?units=si&lang=id'
+// const url = 'https://api.darksky.net/forecast/17ae17408409e68c79f65c4360ca5d53/37.8267,-122.4233'
+
+// request({
+//     url: url,
+//     json: true
+// }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+//     }
+// })
+
+// Geocodeing
+// Input Location and get Latitude & Longitude
+
+const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/jakarta.json?access_token=pk.eyJ1IjoibWNzeWF1cWkiLCJhIjoiY2p1ajdwNmEzMWlwaTN5cGtuNGp4d3B6ZyJ9.wBIJ6cqdaaaqy6FkTiuq3g&limit=1'
 
 request({
     url: url,
     json: true
 }, (error, response) => {
-    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+    if (error) {
+        console.log('Unable to connect')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location')
+    } else {
+        console.log("Longitude: " + response.body.features[0].center[0] + "\nLatitude: " + response.body.features[0].center[1])
+    }
 })
