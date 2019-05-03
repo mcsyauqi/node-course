4ba99654200e07d37eac3a11df54c7b62be968d8
Unknown <ahmadthariqsyauqi@gmail.com>
2019-05-03 15:11:58 +0700

    47. Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,7 +7,28 @@ console.log(path.join(__dirname, '../public'))
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ahmad Thariq Syauqi'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Ahmad Thariq Syauqi'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -15,9 +36,6 @@ app.get('/weather', (req, res) => {
         location: 'Banyumas'
     })
 })
-// app.com
-// app.com/help
-// app.com/about
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
