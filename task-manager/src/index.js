7b6246aaa7f3a7eef376ebf9281a32952c2cd562
Unknown <ahmadthariqsyauqi@gmail.com>
2019-05-13 05:39:17 +0700

    111. Logging Out

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET request are disable')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('We are on maintenance!')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
