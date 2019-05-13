bec3696454972a7293d4265e21cd5267b0907708
Unknown <ahmadthariqsyauqi@gmail.com>
2019-05-13 21:26:07 +0700

    121. Sorting Data

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,14 +7,6 @@ const app = express()
 const port = process.env.PORT || 3000
 
 // app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET request are disable')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
 //     res.status(503).send('We are on maintenance!')
 // })
 
@@ -25,3 +17,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port: ' + port)
 })
+
+// const Task = require('./models/task')
+// const User = require('./models/user')
+
+// const main = async () => {
+//     // const task = await Task.findById('5cd8da420b4354240cbb3bfa')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
+
+//     const user = await User.findById('5cd8da350b4354240cbb3bf7')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
+
+// main()
