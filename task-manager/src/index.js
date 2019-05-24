3b018b26783afd3bdca8ac8ee3196b20387a13ce
Unknown <ahmadthariqsyauqi@gmail.com>
2019-05-24 20:36:39 +0700

    142. Testing an Express Application: Part II

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,18 +1,10 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const app = require('./app')
 
-const app = express()
 const port = process.env.PORT
 
 // app.use((req, res, next) => {
 //     res.status(503).send('We are on maintenance!')
 // })
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port: ' + port)
